72866cf8f297870730788bbdedc085fec5dace4d
MD.Taahaa Bakhshi <taahaabakhshi7970@gamil.com>
2024-12-06 21:55:36 +0530

    changed path

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import  Sidebar  from './components/Sidebar/Sidebar'
 // import Sidebar from './components/Sidebar/Sidebar'
-import Main from './components/main/main'
+import Main from './components/Main/Main'
 
 const App = () => {
   return (
